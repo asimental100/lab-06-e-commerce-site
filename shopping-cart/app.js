5b9c036c1b04ae16e49ef84f7bfbd3c5a1511e85
Adrian Simental <asimental100@hotmail.com>
2020-07-16 17:03:56 -0700

    added functionality to empty cart and place order buttons

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -5,6 +5,11 @@ import { renderTableRow } from './renderCart.js';
 const tableBody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total');
 
+const placeOrderButton = document.getElementById('place-order-button');
+const emptyCartButton = document.getElementById('empty-cart-button');
+
+let orderNumber = Math.floor(Math.random() * 99999999);
+
 let cart = getCart();
 
 for (let i = 0; i < cart.length; i++) {
@@ -21,3 +26,19 @@ for (let i = 0; i < cart.length; i++) {
 
 const orderTotal = calcOrderTotal(cart, currentProducts);
 orderTotalCell.textContent = toUSD(orderTotal);
+
+placeOrderButton.addEventListener('click', () => {
+    let orderConfirmation = confirm('Are you SURE you would like to place an order?');
+    if (orderConfirmation) {
+        alert(`Thank you for your order! Your order confirmation number is: ${orderNumber}`);
+        localStorage.clear();
+        location.reload();
+    } else {
+        prompt('Why Not?');
+    }
+});
+
+emptyCartButton.addEventListener('click', () => {
+    localStorage.clear();
+    location.reload();
+});
